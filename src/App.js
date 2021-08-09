4082da52d2d33835df89ed7f1f1de3ee1db659ab
hugues0 <ntwari.hugues@gmail.com>
2021-08-09 22:12:20 +0200

    navbar almost done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+//import './App.css';
+import Menu from './components/menu/menu'
+import Welcome from './components/welcome/welcome';
 
 function App() {
   return (
     <div className="App">
-      
+      <Menu />
+      {/* <Welcome /> */}
     </div>
   );
 }
---
+++ src/components/welcome/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const welcome = () => {
+    return (
+        <div>
+            <img src="../images/cookingwoman.jpg" alt="cooking" />
+        </div>
+    )
+}
+
+export default welcome
